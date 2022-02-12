810c531f48a338edb37ba0c88575dc848151d4c4
Leonardo <rabelo180@gmail.com>
2022-02-12 19:11:32 -0300

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const postmanRequest = require("postman-request");
+
+const forecast = (latitude, longitude, callback) => {
+	const url = `http://api.weatherstack.com/current?access_key=8524aec26270636156488ac37defcf71&query=${latitude},${longitude}&units=m`;
+
+	postmanRequest({ url, json: true }, (error, { body } = {}) => {
+		if (error) {
+			callback("Unable to connect to weather service!", undefined);
+		} else if (body.error) {
+			callback("Unable to find location.", undefined);
+		} else {
+			callback(
+				undefined,
+				`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. ${body.current.feelslike} feels like degress.`,
+			);
+		}
+	});
+};
+
+module.exports = forecast;
